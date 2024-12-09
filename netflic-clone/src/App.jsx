18c344452620a2effd33cb51a3fdc3a7de571f67
FASILalemu <fasilalemu55@gmail.com>
2024-12-09 19:06:13 +0300

    Header and Footer

--- netflic-clone/src/App.jsx
+++ netflic-clone/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App"> 
-     <h1>Hello</h1>
+     <Home/>
     </div>
   )
 }
